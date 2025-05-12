ee4e1fa5e950af10017301f7ae55fd9b9ad7fe14
Nikolay <30789678+Nikolaiy@users.noreply.github.com>
2025-05-12 08:59:09 +0500

    added function Debug

--- library/mainFunctions.php
+++ library/mainFunctions.php
@@ -17,3 +17,12 @@ function loadTemplate($smarty, $templateName)
 {
     $smarty->display($templateName . TemplatePostfix);
 }
+
+function d($value = null, $die = 1)
+{
+    echo 'Debug: <br /><pre>';
+    print_r($value);
+    echo '</pre>';
+
+    if($die) die;
+}
