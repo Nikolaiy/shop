12529bfbc2c2dbceccb519591181c9f6cbe7d7f1
Nikolay <30789678+Nikolaiy@users.noreply.github.com>
2025-05-12 09:46:38 +0500

    change structure file tpl

--- tmp/smarty/templates_c/864646e6ee363fbcc7c30fb4575049fbbaf685c1_0.file_index.tpl.php
+++ tmp/smarty/templates_c/864646e6ee363fbcc7c30fb4575049fbbaf685c1_0.file_index.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 5.4.5, created on 2025-05-09 10:53:18
+/* Smarty version 5.4.5, created on 2025-05-12 06:23:22
   from 'file:index.tpl' */
 
 /* @var \Smarty\Template $_smarty_tpl */
 if ($_smarty_tpl->getCompiled()->isFresh($_smarty_tpl, array (
   'version' => '5.4.5',
-  'unifunc' => 'content_681dc27e8ddef4_38372827',
+  'unifunc' => 'content_682177ba2370d8_04817911',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '864646e6ee363fbcc7c30fb4575049fbbaf685c1' => 
     array (
       0 => 'index.tpl',
-      1 => 1746779939,
+      1 => 1747023281,
       2 => 'file',
     ),
   ),
@@ -20,15 +20,38 @@ if ($_smarty_tpl->getCompiled()->isFresh($_smarty_tpl, array (
   array (
   ),
 ))) {
-function content_681dc27e8ddef4_38372827 (\Smarty\Template $_smarty_tpl) {
+function content_682177ba2370d8_04817911 (\Smarty\Template $_smarty_tpl) {
 $_smarty_current_dir = 'C:\\xampp\\htdocs\\shop.local\\views\\default';
 ?><html>
     <head>
         <title><?php echo $_smarty_tpl->getValue('pageTitle');?>
 </title>
+        <link rel="stylesheet" href="<?php echo $_smarty_tpl->getValue('templateWebPath');?>
+css/main.css" type="text/css">
     </head>
 <body>
-<?php echo $_smarty_tpl->getValue('pageTitle');?>
+    <div id="header">
+        <h1>my shop - интернет магазин</h1>
+    </div>
+
+    <div id="leftColumn">
+
+        <div id="leftMenu">
+            <div class="menuCaption">Меню:</div>
+                 пункт1<br />
+                 пункт2<br />
+                 пункт3<br />
+        </div>
+
+    </div>
+
+    <div id="centerColumn">
+        centerColumn
+    </div>
+
+    <div id="footer">
+        Footer
+    </div>
 
 </body>
 
--- views/default/index.tpl
+++ views/default/index.tpl
@@ -1,9 +1,32 @@
 <html>
     <head>
         <title>{$pageTitle}</title>
+        <link rel="stylesheet" href="{$templateWebPath}css/main.css" type="text/css">
     </head>
 <body>
-{$pageTitle}
+    <div id="header">
+        <h1>my shop - интернет магазин</h1>
+    </div>
+
+    <div id="leftColumn">
+
+        <div id="leftMenu">
+            <div class="menuCaption">Меню:</div>
+                 пункт1<br />
+                 пункт2<br />
+                 пункт3<br />
+        </div>
+
+    </div>
+
+    <div id="centerColumn">
+        centerColumn
+    </div>
+
+    <div id="footer">
+        Footer
+    </div>
+
 </body>
 
 </html>
